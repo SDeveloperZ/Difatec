cd8324ac6bcca938e082671b9d8f4814b83c4e92
Sergio Lopez <sergiolopez403@gmail.com>
2023-02-14 15:13:58 -0300

    Difatec Equipamientos

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/*submenu */
+const subSubMenu = document.querySelector(".sub-menu2");
+const subMenuLink = document.querySelector(".open-sub-submenu a");
+
+subMenuLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  subSubMenu.style.display = "block";
+});
+
+subSubMenu.addEventListener("mouseout", function(event) {
+  if (!subSubMenu.contains(event.relatedTarget)) {
+    subSubMenu.style.display = "none";
+  }
+});
+
+/*carrusel*/
+const slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showSlides() {
+  slides.forEach(slide => {
+    slide.style.display = "none";
+  });
+  slides[index].style.display = "flex";
+  index++;
+  if (index >= slides.length) {
+    index = 0;
+  }
+  setTimeout(showSlides, 4000);
+}
+
+showSlides();
